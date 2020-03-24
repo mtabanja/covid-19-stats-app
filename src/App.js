b099d2a32fbe73de660298c9946f2c7bea7f53a4
mtabanja <mouaztabanja@outlook.com>
2020-03-24 20:56:03 +0100

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactGA from "react-ga";
 import "./App.css";
 import { Route } from "react-router-dom";
+
 import NavBar from "./components/NavBar";
 import WorldWide from "./components/WorldWide";
 import Country from "./components/Country";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("UA-161774679-1");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className="App">
       <Route path="/" component={NavBar} />
